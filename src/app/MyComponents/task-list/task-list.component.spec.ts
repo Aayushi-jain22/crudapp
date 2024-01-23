53130f6f15d528122133945b89ed2cf9cc77f875
Aayushi-jain22 <91220758+Aayushi-jain22@users.noreply.github.com>
2024-01-23 19:31:21 +0530

    tasklist

--- src/app/MyComponents/task-list/task-list.component.spec.ts
+++ src/app/MyComponents/task-list/task-list.component.spec.ts
@@ -1,14 +1,58 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { TaskListComponent } from './task-list.component';
+
+// describe('TaskListComponent', () => {
+//   let component: TaskListComponent;
+//   let fixture: ComponentFixture<TaskListComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [TaskListComponent],
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(TaskListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'; // Import NoopAnimationsModule
 import { TaskListComponent } from './task-list.component';
+import { TodoService } from '../../services/todo.service';
+import { of } from 'rxjs';
 
-describe('TaskListComponent', () => {
+fdescribe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
+  let todoService: jasmine.SpyObj<TodoService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TaskListComponent],
+  beforeEach(() => {
+    todoService = jasmine.createSpyObj('TodoService', [
+      'deleteTask',
+      'updateTask',
+    ]);
+
+    TestBed.configureTestingModule({
+      //   declarations: [TaskListComponent],
+      imports: [
+        FormsModule,
+        MatButtonModule,
+        MatTableModule,
+        BrowserAnimationsModule,
+        TaskListComponent,
+        NoopAnimationsModule,
+      ],
+      providers: [{ provide: TodoService, useValue: todoService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TaskListComponent);
@@ -18,5 +62,56 @@ describe('TaskListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call deleteTask when delete button is clicked', () => {
+    const index = 0;
+
+    // Mock the deleteTask method in the service
+    todoService.deleteTask.and.returnValue(of(undefined));
+
+    // Trigger the deleteTask method
+    component.deleteTask(index);
+
+    expect(todoService.deleteTask).toHaveBeenCalledWith(
+      component.tasks[index].id
+    );
+  });
+
+  it('should call updateTask when save button is clicked', () => {
+    const index = 0;
+
+    todoService.updateTask.and.returnValue(of(component.tasks[index]));
+
+    component.saveTask(index);
+
+    expect(todoService.updateTask).toHaveBeenCalledWith(
+      component.tasks[index].id,
+      component.tasks[index]
+    );
+  });
+
+  it('should toggle edit mode when edit button is clicked', () => {
+    const index = 0;
+
+    expect(component.tasks[index].editMode).toBeFalsy();
+
+    component.toggleEditMode(index);
+
+    // After toggle, editMode should be true
+    expect(component.tasks[index].editMode).toBeTruthy();
+  });
+
+  it('should toggle task completion when mark completed button is clicked', () => {
+    const index = 0;
+
+    // Initial completed status should be false
+    expect(component.tasks[index].completed).toBeFalsy();
+
+    // Trigger the markCompleted method
+    component.markCompleted(index);
+
+    // After toggle, completed status should be true
+    expect(component.tasks[index].completed).toBeTruthy();
   });
 });
